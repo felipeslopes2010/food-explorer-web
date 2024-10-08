93322f6dc1e995158d5ab3012b26777ad6b2e36a
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-08 15:31:01 -0300

    Add: Dessert section in Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,13 @@ import { HeaderCostumer } from "../../components/HeaderCostumer";
 import { ProductCard } from "../../components/ProductCard";
 import bannerImg from "../../assets/macaron-banner.png"
 import salada from "../../assets/salada.png"
+import torradas from "../../assets/torradas.png"
+import salada2 from "../../assets/salada2.png"
 import spaguetti from "../../assets/spaguetti.png"
+import prugna from "../../assets/prugna.png"
+import peachy from "../../assets/peachy.png"
+import macarons from "../../assets/macarons.png"
+import boloDamasco from "../../assets/bolo-damasco.png"
 
 import { Container, Main, Banner, Products } from "./styles";
 
@@ -29,33 +35,70 @@ export function Home() {
                 <ProductCard
                     image={salada}
                     name="Salada Ravanello"
-                    description="Rabanetes, folhas verdes e molho agridoce salpicados com gergelim"
+                    description="Rabanetes, folhas verdes e molho agridoce salpicados com gergelim."
                     priece="R$ 49,97"
                 />
 
                 <ProductCard
                     image={spaguetti}
                     name="Spaguetti Gambe"
-                    description="Massa fresca com camarões e pesto. "
+                    description="Massa fresca com camarões e pesto."
                     priece="R$ 79,97"
                 />
 
                 <ProductCard
-                    image={salada}
-                    name="Salada Ravanello"
-                    description="Rabanetes, folhas verdes e molho agridoce salpicados com gergelim"
+                    image={torradas}
+                    name="Torradas de Parma"
+                    description="Presunto de parma e rúcula em um pão com fermentação natural."
                     priece="R$ 49,97"
                 />
 
                 <ProductCard
-                    image={salada}
-                    name="Salada Ravanello"
-                    description="Rabanetes, folhas verdes e molho agridoce salpicados com gergelim"
-                    priece="R$ 49,97"
+                    image={salada2}
+                    name="Salada Caesar"
+                    description="Salada clássica e popular que consiste em alface romana, croutons e queijo parmesão "
+                    priece="R$ 35,97"
                 />
 
             <IoIosArrowForward />
             </Products>
+
+            <h2>Sobremesas</h2>
+
+            <Products>
+                <IoIosArrowBack />
+                <ProductCard
+                    image={prugna}
+                    name="Prugna Pie"
+                    description="Torta de ameixa com massa amanteigada, polvilho em açúcar."
+                    priece="R$ 79,97"
+                />
+
+                <ProductCard
+                    image={peachy}
+                    name="Peachy pastrie"
+                    description="Delicioso folheado de pêssego com folhas de hortelã."
+                    priece="R$ 32,97"
+                />
+
+                <ProductCard
+                    image={macarons}
+                    name="Macarons"
+                    description="Farinha de amêndoas, manteiga, claras e açúcar."
+                    priece="R$ 79,97"
+                />
+
+                <ProductCard
+                    image={boloDamasco}
+                    name="Bolo de damasco"
+                    description="Bolo de chocolate com damascos secos e picados e açúcar de confeiteiro"
+                    priece="R$ 69,97"
+                />
+
+            <IoIosArrowForward />
+            </Products>
+
+
         </Main>
 
         </Container>
