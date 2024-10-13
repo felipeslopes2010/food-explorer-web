ac60f45c812d77d2cc02e248857121459ee6bc29
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-13 20:07:59 -0300

    Add: Create EditFood page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { NewFood } from './pages/NewFood'
+import { EditFood } from './pages/EditFood'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <NewFood />
+        <EditFood />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/EditFood/index.jsx
@@ -0,0 +1,83 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { FiUpload } from "react-icons/fi";
+
+import { TextArea } from "../../components/TextArea";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { Link } from "../../components/Link";
+
+import { Container, Main, Form, InputWrapper, ButtonWrapper } from "./styles"
+
+export function EditFood() {
+    return (
+        <Container>
+            <Header />
+            <Main>
+                <Link
+                    icon={IoIosArrowBack}
+                    title="voltar"
+                />
+
+                <h1>Editar prato</h1>
+
+                <Form>
+                    <InputWrapper>
+                        <label>Imagem do prato</label>
+                        <Input
+                            icon={FiUpload}
+                            placeholder="Selecione imagem"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <label>Nome</label>
+                        <Input
+                            placeholder="Ex.: Salada Ceasar"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <label>Categoria</label>
+                        <Input
+                            placeholder="Refeição"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <label>Ingredientes</label>
+                        <Input
+                            placeholder="Refeição"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <label>Preço</label>
+                        <Input
+                            placeholder="R$ 00,00"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <label>Descrição</label>
+                        <TextArea
+                            placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+                        />
+                    </InputWrapper>
+
+                    <ButtonWrapper>
+                        <Button
+                            title="Excluir prato"
+                        />
+
+                        <Button
+                            title="Salvar alterações"
+                        />
+                    </ButtonWrapper>
+                </Form>
+            </Main>
+            <Footer />
+        </Container>
+    )
+}
