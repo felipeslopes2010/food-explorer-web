68175156f2e273e781b358d90f52ae4dd42b8c17
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-23 15:57:32 -0300

    Add: Create user SignUp integration with backend

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,26 +1,65 @@
-import { Input } from "../../components/Input"
-import { Button } from "../../components/Button"
-import { Link } from "../../components/Link"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Link } from "../../components/Link";
 
 import { Container, InputWrapper, Logo, Form } from "./styles";
 
 export function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const user = {
+        name,
+        email,
+        password,
+    };
+
+    function handleSignUp(event) {
+        event.preventDefault();
+
+        if (!name || !email || !password) {
+            return alert("Preencha todos os campos!");
+        }
+
+        api.post("/users", user)
+            .then(() => {
+                alert("Usuário cadastrado com sucesso!");
+                navigate("/");
+            })
+            .catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message);
+                } else {
+                    alert("Não foi possível cadastrar");
+                }
+            });
+    }
+
     return (
         <Container>
             <Logo>
                 <img src="../src/assets/polygon.svg" alt="Logo Food Explorer" />
                 <h1>food explorer</h1>
             </Logo>
-            <Form>
+            <Form onSubmit={handleSignUp}>
                 <h2>Crie sua conta</h2>
 
                 <InputWrapper>
-                    <label htmlFor="username">Seu nome</label>
+                    <label htmlFor="name">Seu nome</label>
                     <Input
-                        id="username"
+                        id="name"
                         placeholder="Exemplo: Maria da Silva"
                         type="text"
                         maxLength="30"
+                        onChange={e => setName(e.target.value)}
                     />
                 </InputWrapper>
 
@@ -31,8 +70,9 @@ export function SignUp() {
                         placeholder="Exemplo: exemplo@exemplo.com.br"
                         type="email"
                         maxLength="30"
+                        onChange={e => setEmail(e.target.value)}
                     />
-                  </InputWrapper>
+                </InputWrapper>
 
                 <InputWrapper>
                     <label htmlFor="password">Senha</label>
@@ -41,14 +81,16 @@ export function SignUp() {
                         placeholder="No mínimo 6 caracteres"
                         type="password"
                         maxLength="30"
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </InputWrapper>
 
-                <Button title="Criar conta" />
-
-                <Link to="/"
-                    title="Já tenho uma conta"
+                <Button
+                    title="Criar conta"
+                    type="submit"
                 />
+
+                <Link to="/" title="Já tenho uma conta" />
             </Form>
         </Container>
     );
