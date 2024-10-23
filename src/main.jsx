c89ff618af91846a5e6c0af0f8afc008582c3928
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-10-22 21:39:01 -0300

    Add: Create React Routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { NewFood } from './pages/NewFood'
-import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+
+import { Routes } from './routes'
+
 import theme from './styles/theme'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <NewFood />
+        <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { NewFood } from "../pages/NewFood";
+import { EditFood } from "../pages/EditFood";
+import { Details } from "../pages/Details";
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<NewFood />} />
+            <Route path="/edit/:id" element={<EditFood />} />
+            <Route path="/details/:id" element={<Details />} />
+        </Routes>
+    )
+}
