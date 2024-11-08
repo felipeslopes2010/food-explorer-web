7582193676c17a0a02e1b758d3c6f1bc030f83e2
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-08 00:45:37 -0300

    Add: Edit Food page loads dishes data

--- src/pages/EditFood/index.jsx
+++ src/pages/EditFood/index.jsx
@@ -1,6 +1,12 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import { IoIosArrowBack } from "react-icons/io";
 import { FiUpload } from "react-icons/fi";
 
+import { api } from "../../services/api";
+
+import { IngredientsItem } from "../../components/IngredientsItem";
 import { TextArea } from "../../components/TextArea";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
@@ -8,9 +14,76 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Link } from "../../components/Link";
 
-import { Container, Main, Form, InputWrapper, ButtonWrapper } from "./styles"
+import { Container, Main, Form, InputWrapper, ButtonWrapper } from "./styles";
 
 export function EditFood() {
+    const [data, setData] = useState(null);
+    const [fileName, setFileName] = useState("");
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const [newIngredient, setNewIngredient] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+
+    const params = useParams();
+
+    function handleAddIngredient() {
+        if (newIngredient.trim()) {
+            setIngredients(prevState => [...prevState, { name: newIngredient }]);
+            setNewIngredient("");
+        }
+    }
+
+    function handleRemoveIngredient(deletedName) {
+        setIngredients(prevState => prevState.filter(ingredient => ingredient.name !== deletedName));
+    }
+
+    async function handleUpdateDish(e) {
+        e.preventDefault();
+
+        if(!name) {
+            return alert("Nome obrigatório!");
+        } else if(!category) {
+            return alert("Categoria obrigatória!");
+        } else if(ingredients.length === 0) {
+            return alert("Informe pelo menos um ingrediente!");
+        } else if(newIngredient) {
+            return alert("Você deixou um ingrediente no campo para adicionar, mas não clicou em adicionar! Clique para adicionar ou deixe o campo vazio.");
+        } else if(!price) {
+            return alert("Preço obrigatório!");
+        } else if(!description) {
+            return alert("Descrição obrigatória!");
+        }
+
+        await api.put(`/dishes/${params.id}`, {
+            image: fileName,
+            name,
+            category,
+            ingredients,
+            price,
+            description
+        });
+        
+        alert("Prato atualizado com sucesso!");
+        navigate("/");
+    }
+
+    useEffect(() => {
+        async function fetchDish() {
+            const response = await api.get(`/dishes/${params.id}`);
+            setData(response.data);
+            setFileName(response.data.image);
+            setName(response.data.name);
+            setCategory(response.data.category);
+            setIngredients(response.data.ingredients);
+            setPrice(response.data.price);
+            setDescription(response.data.description);
+        }
+
+        fetchDish();
+    }, []);
+
     return (
         <Container>
             <Header />
@@ -22,62 +95,86 @@ export function EditFood() {
 
                 <h1>Editar prato</h1>
 
-                <Form>
-                    <InputWrapper>
-                        <label>Imagem do prato</label>
-                        <Input
-                            icon={FiUpload}
-                            placeholder="Selecione imagem"
-                        />
-                    </InputWrapper>
+                {data && (
+                    <Form>
+                        <InputWrapper>
+                            <label>Imagem do prato</label>
+                            <Input
+                                icon={FiUpload}
+                                placeholder="Selecione imagem"
+                                value={fileName}
+                                onChange={e => setFileName(e.target.value)}
+                            />
+                        </InputWrapper>
 
-                    <InputWrapper>
-                        <label>Nome</label>
-                        <Input
-                            placeholder="Ex.: Salada Ceasar"
-                        />
-                    </InputWrapper>
+                        <InputWrapper>
+                            <label>Nome</label>
+                            <Input
+                                value={name}
+                                placeholder="Ex.: Salada Ceasar"
+                                onChange={e => setName(e.target.value)}
+                            />
+                        </InputWrapper>
 
-                    <InputWrapper>
-                        <label>Categoria</label>
-                        <Input
-                            placeholder="Refeição"
-                        />
-                    </InputWrapper>
+                        <InputWrapper>
+                            <label>Categoria</label>
+                            <Input
+                                value={category}
+                                placeholder="Refeição"
+                                onChange={e => setCategory(e.target.value)}
+                            />
+                        </InputWrapper>
 
-                    <InputWrapper>
-                        <label>Ingredientes</label>
-                        <Input
-                            placeholder="Refeição"
-                        />
-                    </InputWrapper>
+                        <InputWrapper>
+                            <label>Ingredientes</label>
+                            <Input>
+                                {ingredients.map((ingredient, index) => (
+                                    <IngredientsItem
+                                        key={String(index)}
+                                        value={ingredient.name}
+                                        onClick={() => handleRemoveIngredient(ingredient.name)}
+                                    />
+                                ))}
+                                <IngredientsItem
+                                    isnew
+                                    placeholder="Adicionar"
+                                    value={newIngredient}
+                                    onChange={e => setNewIngredient(e.target.value)}
+                                    onClick={handleAddIngredient}
+                                />
+                            </Input>
+                        </InputWrapper>
 
-                    <InputWrapper>
-                        <label>Preço</label>
-                        <Input
-                            placeholder="R$ 00,00"
-                        />
-                    </InputWrapper>
+                        <InputWrapper>
+                            <label>Preço</label>
+                            <Input
+                                value={price}
+                                placeholder="R$ 00,00"
+                                onChange={e => setPrice(e.target.value)}
+                            />
+                        </InputWrapper>
 
-                    <InputWrapper>
-                        <label>Descrição</label>
-                        <TextArea
-                            placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
-                        />
-                    </InputWrapper>
+                        <InputWrapper>
+                            <label>Descrição</label>
+                            <TextArea
+                                value={description}
+                                placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+                                onChange={e => setDescription(e.target.value)}
+                            />
+                        </InputWrapper>
 
-                    <ButtonWrapper>
-                        <Button
-                            title="Excluir prato"
-                        />
+                        <ButtonWrapper>
+                            <Button title="Excluir prato" />
 
-                        <Button
-                            title="Salvar alterações"
-                        />
-                    </ButtonWrapper>
-                </Form>
+                            <Button
+                                title="Salvar alterações"
+                                onClick={handleUpdateDish}
+                            />
+                        </ButtonWrapper>
+                    </Form>
+                )}
             </Main>
             <Footer />
         </Container>
-    )
+    );
 }
